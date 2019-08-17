bd8e8b27bf7fa285a740ae7ff6e8308c52c7ff43
alibek <a.kazhibekov@mail.ru>
2019-08-18 01:14:49 +0600

    task 3.1

--- index.php
+++ index.php
@@ -109,40 +109,5 @@ for ($comments = []; $row = mysqli_fetch_assoc($result); $comments[] = $row);
         </main>
     </div>
 </body>
-<!-- <body>
-	
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h1>User management</h1>
-				<a href="create.html" class="btn btn-success">Add User</a>
-				
-				<table class="table">
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Username</th>
-							<th>Email</th>
-							<th>Actions</th>
-						</tr>
-					</thead>
 
-					<tbody>
-                        <?php foreach ($users as $user) { ?>
-						<tr>
-							<td><?php echo $user['id'] ?></td>
-							<td><?php echo $user['name'] ?></td>
-							<td><?php echo $user['email'] ?></td>
-							<td>
-								<a href="edit.html" class="btn btn-warning">Edit</a>
-								<a href="#" onclick="return confirm('are you sure?')" class="btn btn-danger">Delete</a>
-							</td>
-                        </tr>
-                        <?php }?>						
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</body> -->
 </html>
